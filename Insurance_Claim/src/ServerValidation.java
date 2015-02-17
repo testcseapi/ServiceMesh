43b05ecef4655f58c7db7b93b2d67a20e8a7da85
keerthi <adeenadayala@csc.com>
2015-02-17 16:45:09 +0530

    Service Mesh Successfull Case

--- Insurance_Claim/src/ServerValidation.java
+++ Insurance_Claim/src/ServerValidation.java
@@ -67,7 +67,7 @@ public class ServerValidation extends HttpServlet {
 		
 		boolean result = true;
 		
-    ////AppWorks Logical Error Introduce
+    //AppWorks Logical Error Introduce
 	 //  if (ClaimSubmit == "") {
 		if (ClaimSubmit.equals("")) {
 			error += "Please Select the ClaimSubmit<br/>";
